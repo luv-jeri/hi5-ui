38e55d3785580f82cac2d0a1b7362164716e412f
BRIJESH PRAJAPATI <userbrijesh@gmail.com>
2022-08-06 19:39:58 +0530

    login ui added

--- src/pages/auth/Join.jsx
+++ src/pages/auth/Join.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { useState } from 'react';
 import useAuth from '../../context/Auth.context';
+import { useNavigate } from 'react-router-dom';
 import { TextInput, Container, Text, Button, Checkbox } from '@mantine/core';
+import logo from '../.././assets/images/hi5.png';
+import  '../.././styles/pages/login.css';
+import  '../.././styles/util.css';
+
 
 export default function SingUp() {
   const { signUp } = useAuth();
+  const navigate = useNavigate();
 
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
@@ -23,8 +29,21 @@ export default function SingUp() {
   };
 
   return (
-    <Container className='f f-c ai-c jc-c container'>
-      <Text size='xl' weight={700}>
+    <div className="s-main m-t-10">
+
+   
+   <div className='s-container f jc-c ai-c m-t-10'>
+    <div className='s-content f-1'>
+        <img src={logo} alt="logo" />
+        <p>"First Chat App With Amazing Features, 
+      Connect with people whome you want to"</p>
+    </div>
+    <div className='sign f-2'>
+    <Container className='f f-c ai-c jc-c container' style={{
+        width: '40vw',
+       
+      }}>
+      <Text size='xl' weight={700} >
         Join Us!
       </Text>
       <TextInput
@@ -88,13 +107,46 @@ export default function SingUp() {
       />
 
       <Button
-        variant='gradient'
+   
         className='m-t-10'
-        gradient={{ from: 'indigo', to: 'cyan' }}
+        variant='gradient'
+        gradient={{ from: '#0fb219', to: 'rgba(200, 246, 196, 1)' }}
+        style={{
+          width: '100%',
+          height: '3rem',
+          textAlign: 'center',
+          border: '1px #FEFBF6 solid',
+          
+          paddingBlock: '0.5rem',
+          borderRadius: '0.5rem',
+          cursor: 'pointer',
+          fontSize: '1.3rem',
+          
+        }}
         onClick={handleClick}
       >
         Sign Up
       </Button>
+      <Text
+        style={{
+          marginTop: '1rem',
+          textAlign: 'center',
+          border: '1px #FEFBF6 solid',
+          paddingBlock: '0.5rem',
+          borderRadius: '0.5rem',
+          cursor: 'pointer',
+          
+        }}
+        onClick={() => {
+          navigate('/');
+        }}
+      >
+        <p>Already Registered? <a>  LOGIN NOW</a></p>
+        
+      </Text>
     </Container>
+    </div>
+   </div>
+   </div>
   );
 }
